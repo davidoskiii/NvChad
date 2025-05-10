8cce70c4114b4c65091c5120f83c55ebf66108f0
davidoskiii <davide.mascherin9@gmail.com>
2025-05-10 19:07:54 +0200

    New update

---
+++ lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp" , "prettier",
+        "clangd", "rust-analyzer"
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua-language-server", "stylua", "html-lsp", "css-lsp", "prettier", "clangd" },
+    }
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+}
